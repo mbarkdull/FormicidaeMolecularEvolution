401bf3b1905d116d4a7be6859a24ae0fe2ae6e74
mbarkdull <mb2337@cornell.edu>
2020-11-03 10:27:06 -0500

    Write a for loop to iterate the filtering function over all species.

--- scripts/FilteringCDSbyMSA.R
+++ scripts/FilteringCDSbyMSA.R
@@ -1,8 +1,12 @@
-library(phylotools)
-library(tidyverse)
-library(plyr)
+# Read in the input data:
+# The only thing left to do is figure out how to assign this at the command line:
+speciesInfo <- read.table(file = "./scripts/inputurls_partial", sep = ",")
+# Split the second column to get a column with only abbreviations:
+speciesInfo <- separate(data = speciesInfo, col = V2, into = c("abbrev", "transcript"), sep = "_")
+# Get a vector from that column:
+abbreviations <- speciesInfo$abbrev
 
-# I've written a function to do what I want:
+# I've written this function that will produce a subsetted nucleotide sequence file:
 cdsFiltering <- function(cdsFile, msaFile, msaPrefix, output) {
   # Load packages:
   library(phylotools)
@@ -22,4 +26,20 @@ cdsFiltering <- function(cdsFile, msaFile, msaPrefix, output) {
   dat2fasta(FilteredCodingSequences, outfile = output)
 }
 
-# Now I need to run this iteratively based on command line input:
+# This while loop iterates the subsetting function on all species:
+for (i in abbreviations)
+{
+  print(i)
+  # The last thing I'll need to change is to make sure the paths to these things are correct in the context of the whole directory.
+  cdsFile <- (paste("cds_", i, "_transcripts.fasta", sep = ""))
+  print(cdsFile)
+  msaFile <- (paste("proteins_", i, ".fasta", sep = ""))
+  print(msaFile)
+  msaPrefix <- (paste(i, "_transcripts_", sep = ""))
+  print(msaPrefix)
+  output <- (paste("filtered_", i, "_cds.fasta", sep = ""))
+  print(output)
+  
+  cdsFiltering(cdsFile = cdsFile, msaFile = msaFile, msaPrefix = msaPrefix, output = output)
+  }
+
