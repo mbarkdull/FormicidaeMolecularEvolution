adc19f9f32c387d03f69a345701754a2cf443457
mbarkdull <mb2337@cornell.edu>
2020-11-19 10:42:00 -0500

    Adding an additional section that will construct a pruned tree with tip labels that correspond to filenames (e.g. acep_transcripts) rather than to species names (e.g. Acromyrmex echinator). This output, FilenameTree.tre, will be input to Orthofinder as the correct species tree.

--- scripts/DataTree.R
+++ scripts/DataTree.R
@@ -8,7 +8,7 @@ if (length(args)==0) {
   args[2] = "out.txt"
 }
 
-# This script will generate a tree for BUSTED based on an input tree and a set of tips to retain. 
+# This script will generate a tree for Orthofinder and BUSTED based on an input tree and a set of tips to retain. 
 # The command to run this is `Rscript ./scripts/DataTree.R [path to full tree] [path to inputurls file]`
 
 library(ape)
@@ -21,11 +21,25 @@ FullTree <- read.tree(file = args[1])
 speciesInfo <- read.table(file = args[2], sep = ",")
 StudySpecies <- speciesInfo$V3
 
-# Construct the pruned tree:
-StudyTree <- keep.tip(FullTree, StudySpecies)
+#Construct a vector of file names corresponding to the species (for use with Orthofinder, and potentially with BUSTED)
+# Split the second column by period (.) to get a column with just abbrev_transcripts:
+speciesInfo <- separate(data = speciesInfo, col = V2, into = c("transcripts", ".fasta"), sep = "\\.")
+filenames <- speciesInfo$transcripts
 
-# Export the pruned tree:
+# Construct the species pruned tree:
+StudyTree <- keep.tip(FullTree, StudySpecies)
+# Export the species pruned tree:
 write.tree(StudyTree, file = "./Tree/StudyTree.tre")
 
+# Construct the filename pruned tree:
+rename.tips <- function(phy, old_names, new_names) {
+  mpos <- match(old_names,phy$tip.label)
+  phy$tip.label[mpos] <- new_names
+  return(phy)
+}
 
+FilenameTree <-  StudyTree
+FilenameTree <-  rename.tips(phy = FilenameTree, old_names = StudySpecies, new_names = filenames)
+# Export the filename pruned tree:
+write.tree(FilenameTree, file = "./Tree/FilenameTree.tree")
 
