ce289b029db4a6d1380f3fa09ef671778d1944a2
mbarkdull <mb2337@cornell.edu>
2020-11-20 12:33:11 -0500

    Adding prefixes to directories to make them more readable.

--- scripts/DataMSA.R
+++ scripts/DataMSA.R
@@ -5,10 +5,10 @@ if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 } else if (length(args)==1) {
   # default output file
-  args[2] = "/workdir/mb2337/FormicidaeMolecularEvolution/OrthoFinder/fasta/OrthoFinder/Results_*/MultipleSequenceAlignments"
+  args[2] = "/workdir/mb2337/FormicidaeMolecularEvolution/5_OrthoFinder/fasta/OrthoFinder/Results_*/MultipleSequenceAlignments"
 }
 
-# The command to run this script is `Rscript ./scripts/DataMSA.R [inputurls file] [path to Orthofinder MSA files]`, for example: `./scripts/DataMSA.R ./scripts/inputurls_partial /workdir/mb2337/FormicidaeMolecularEvolution/OrthoFinder/fasta/OrthoFinder/Results_Oct26/MultipleSequenceAlignments`
+# The command to run this script is `Rscript ./scripts/DataMSA.R [inputurls file] [path to Orthofinder MSA files]`, for example: `./scripts/DataMSA.R ./scripts/inputurls_partial /workdir/mb2337/FormicidaeMolecularEvolution/5_OrthoFinder/fasta/OrthoFinder/Results_Oct26/MultipleSequenceAlignments`
 
 # This script will reshuffle the Orthofinder MSA files so that instead of one file per orthogroup with all species present, we have one file per specise with all orthogroups present. 
 
@@ -24,10 +24,10 @@ speciesInfo <- separate(data = speciesInfo, col = V2, into = c("abbrev", "transc
 abbreviations <- speciesInfo$abbrev
 
 # First make a working directory and copy the folder with the MSA files there (THIS WILL NEED TO BE A VALUE SET ON THE COMMAND LINE). 
-dir.create("./SpeciesMSA")
-file.copy(args[2], "./SpeciesMSA", recursive = TRUE)
+dir.create("./6_SpeciesMSA")
+file.copy(args[2], "./6_SpeciesMSA", recursive = TRUE)
 # Concatenate all of the MSA files into a single file:
-setwd("./SpeciesMSA/MultipleSequenceAlignments")
+setwd("./6_SpeciesMSA/MultipleSequenceAlignments")
 msaFiles <- list.files(pattern = "*.fa")
 allMSAFiles <- bind_rows(lapply(msaFiles, read.fasta))
 
