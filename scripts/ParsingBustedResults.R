3219dc99dea69f5109bb387da7bc8705f2fee7a6
mbarkdull <mb2337@cornell.edu>
2021-07-20 12:20:29 -0400

    This script reads in the results JSON files produced by BUSTED, and creates a list of orthogroups with evidence for positive selection somewhere in the tree.

---
+++ scripts/ParsingBustedResults.R
@@ -0,0 +1,4 @@
+# This script reads in the results JSON files produced by BUSTED, and creates a list of orthogroups with evidence for positive selection somewhere in the tree. 
+
+
+bustedResults <- fromJSON(file = "/Users/meganbarkdull/mb2337/FormicidaeMolecularEvolution/8_3_BustedResults/OG0012635_busted.json")
